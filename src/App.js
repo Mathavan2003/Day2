fc8568508b8999d17af89095bfa81e02411eb4b5
Mathavan2003 <108514155+Mathavan2003@users.noreply.github.com>
2023-04-05 11:04:55 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+import { Header } from './startup';
+import { Products } from './startup';
+import { AboutUs } from './startup';
+
+function App() {
+  return (
+    <>
+    <Header/>
+    <Products/>
+    <AboutUs/>
+    </>
+  );
+}
+
+export default App;
